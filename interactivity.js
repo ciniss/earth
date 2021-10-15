bc52266f44b803916f134b11314c358e22e1e1b5
cinis <ciniss@github.com>
2021-10-15 15:39:11 +0200

    Posting is actually not looking that bad

--- interactivity.js
+++ interactivity.js
@@ -7,13 +7,38 @@ let usernameInput = document.querySelector('#username');
 
 let postDiv = document.querySelector('#posts');
 
-function refreshTweets(){
-    let post = document.createElement('p');
-    let tweet = tweets[tweets.length-1];
-    post.innerHTML = `${tweet.text}. by: ${tweet.username}`;
-    postDiv.appendChild(post);
-}
+/*
+<div class= "post">
+    <div class = 'message'>
+        <p>Humans are the cancer of EARTH!!!!!</p>
+    </div>
+    <div class ='message-footer'>
+        <p>metacloser</p>
+    </div>
+</div>
+                    */ 
 
+
+function refreshTweets(){
+    let innerMessage = document.createElement('p');
+    innerMessage.innerHTML = tweets[tweets.length-1].text;
+    let message = document.createElement('div');
+    message.className = 'message';
+    message.appendChild(innerMessage);
+    innerMessage = document.createElement('p');
+    innerMessage.innerHTML = tweets[tweets.length-1].username;
+    let messageFooter = document.createElement('div');
+    messageFooter.className = 'message-footer';
+    messageFooter.appendChild(innerMessage);
+
+    let final  = document.createElement('div');
+    final.className = 'post';
+    final.appendChild(message);
+    final.appendChild(messageFooter);
+    let where = document.querySelector('#posts');
+    where.appendChild(final);
+    
+}
 function getData(){
     
     let text = textInput.value;
